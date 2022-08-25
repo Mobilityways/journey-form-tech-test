98f091b9f2f3ec0b4423660f1fbfd223f7e6a20f
Kane Clover <kane.clover@gmail.com>
2022-08-25 22:37:33 +0100

    feat(add-journey): map markers attached to state

--- src/screens/add-journey/AddJourney.tsx
+++ src/screens/add-journey/AddJourney.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@mui/material";
-import { AddJourneyForm } from "./AddJourneyForm";
 import { GoogleMapWrapper } from "../../components/atoms/GoogleMapWrapper";
 import { GoogleMap } from "../../components/atoms/GoogleMap";
 import { Marker } from "../../components/atoms/Marker";
+import { LatLng } from "../../types";
+import { GeoLocationInput } from "../../components/molecules/GeoLocationInput";
 
 export const AddJourney = () => {
+  const [origin, setOrigin] = useState<LatLng | null>(null);
+  const [destination, setDestination] = useState<LatLng | null>(null);
+
   return (
     <Grid container sx={{ height: "100vh" }}>
       <Grid item md={5}>
-        <AddJourneyForm />
+        <form>
+          <GeoLocationInput setLocation={setOrigin} label="Origin" />
+          <GeoLocationInput setLocation={setDestination} label="Destination" />
+          <pre>{JSON.stringify({ origin, destination }, null, 2)}</pre>
+        </form>
       </Grid>
       <Grid item md={7}>
         <GoogleMapWrapper>
           <GoogleMap center={{ lat: 53.4462023, lng: -2.2649167 }} zoom={12}>
-            <Marker position={{ lat: 53.4462023, lng: -2.2649167 }} />
+            <Marker position={origin} />
+            <Marker position={destination} />
           </GoogleMap>
         </GoogleMapWrapper>
       </Grid>
